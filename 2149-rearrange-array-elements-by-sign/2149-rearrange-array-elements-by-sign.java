198d7ac13b2703b1dbc8c8ea4f59a7a23168a71b
Sreedhar <157798473+ThirupatiSreedhar@users.noreply.github.com>
2026-08-04 21:54:02 +0530

    Time: 4 ms (41.52%), Space: 175.4 MB (70.15%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,27 +1,39 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-    int p[]= new int[nums.length/2];
-    int n[]=new int[nums.length/2];
-    int pi=0;
-    int ni=0;
+    // int p[]= new int[nums.length/2];
+    // int n[]=new int[nums.length/2];
+    // int pi=0;
+    // int ni=0;
+    // for(int i=0;i<nums.length;i++){
+    //     if(nums[i]>0){
+    //         p[pi]=nums[i];
+    //         pi++;
+        
+    //     }else{
+    //         n[ni]=nums[i];
+    //         ni++;
+
+    //     }
+    // }  
+    // int j=0;
+    // for(int i=0;i<=p.length-1;i++){
+    //     nums[j]=p[i];
+    //     j++;
+    //     nums[j]=n[i];
+    //     j++;
+    // }  
+    int ans[] = new int[nums.length];
+    int e=0;//even
+    int o=1;//odd
     for(int i=0;i<nums.length;i++){
         if(nums[i]>0){
-            p[pi]=nums[i];
-            pi++;
-        
+            ans[e]=nums[i];
+            e=e+2;
         }else{
-            n[ni]=nums[i];
-            ni++;
-
+            ans[o]=nums[i];
+            o=o+2;
         }
-    }  
-    int j=0;
-    for(int i=0;i<=p.length-1;i++){
-        nums[j]=p[i];
-        j++;
-        nums[j]=n[i];
-        j++;
-    }  
-    return nums;
+    }
+    return ans;
     }
 }

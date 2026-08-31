6af8adce9b7baa1686e92bc30350bd2437f8a0aa
Sreedhar <157798473+ThirupatiSreedhar@users.noreply.github.com>
2026-08-31 21:46:29 +0530

    Time: 0 ms (100%), Space: 47.4 MB (32.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,30 @@
 class Solution {
     public int missingNumber(int[] nums) {
-     HashSet<Integer> hs = new HashSet<>();
-     for(int i :nums){
-        hs.add(i);
-     }
-     for(int i=0;i<nums.length+1;i++){
-        if(!hs.contains(i)){
-            return i;
-        }
+    //  HashSet<Integer> hs = new HashSet<>();
+    //  for(int i :nums){
+    //     hs.add(i);
+    //  }
+    //  for(int i=0;i<nums.length+1;i++){
+    //     if(!hs.contains(i)){
+    //         return i;
+    //     }
     
-     }
-      return 1;
+    //  }
+    //   return 1;
+   
+        int xor = 0;
+
+        // Loop 1: 0 to n
+        for (int i = 0; i <= nums.length; i++) {
+            xor = xor ^ i;
+        }
+
+        // Loop 2: array elements
+        for (int i = 0; i < nums.length; i++) {
+            xor = xor ^ nums[i];
+        }
+
+        return xor;
     }
 
   
